a24e8088967b2d8bfee6eff17e4831fae2b73121
le001 <1512288930@qq.com>
2025-12-05 22:33:51 +0800

    测试

---
+++ 1.数据类型/数据类型.java
@@ -0,0 +1,11 @@
+public class 数据类型
+{
+    public static void main(String[] args)
+    {
+        int intValue = 100;
+        float floatValue = 200;
+        char charValue = 'c';
+        boolean flag = false;
+        数据类型 test = new 数据类型();
+    }
+}
